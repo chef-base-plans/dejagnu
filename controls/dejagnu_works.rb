eb22a97ff160d56070cf71352ca69c32b78a808e
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-26 10:40:48 +0100

    Update documentation
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/dejagnu_works.rb
+++ controls/dejagnu_works.rb
@@ -7,9 +7,9 @@ control 'core-plans-dejagnu-works' do
   impact 1.0
   title 'Ensure dejagnu works as expected'
   desc '
-  Verify dejagnu by ensuring 
-  (1) its installation directory exists and 
-  (2) that it returns the expected version.
+  Verify dejagnu by ensuring that
+  (1) its installation directory exists 
+  (2) it returns the expected version
   '
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
@@ -20,7 +20,7 @@ control 'core-plans-dejagnu-works' do
   end
   
   command_relative_path = input('command_relative_path', value: 'bin/runtest')
-  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  command_full_path = File.join(plan_installation_directory.stdout.strip, command_relative_path)
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
   describe command("#{command_full_path} --version") do
     its('exit_status') { should eq 0 }
